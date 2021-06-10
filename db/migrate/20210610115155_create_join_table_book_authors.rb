8e332274fc89fd0c2b49c7511a68a676b931a6be
ashishvalu <valuashish97@gmail.com>
2021-06-10 17:26:50 +0530

    dbmigration add

---
+++ db/migrate/20210610115155_create_join_table_book_authors.rb
@@ -0,0 +1,4 @@
+class CreateJoinTableBookAuthors < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_133253) do
+ActiveRecord::Schema.define(version: 2021_06_10_115155) do
 
   create_table "customers_products", id: false, force: :cascade do |t|
     t.integer "customer_id", null: false
